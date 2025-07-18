241f8dd0b728b5901d4473018b63e7e9829e06ad
waregagbagbo <gabelwarega@gmail.com>
2025-07-18 13:51:20 +0300

    added a new app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Author(models.Mode):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+    
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
