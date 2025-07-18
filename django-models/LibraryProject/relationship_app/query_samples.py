d344e89dc719bb7b0600ab393228d0e0041da86c
waregagbagbo <gabelwarega@gmail.com>
2025-07-18 16:22:59 +0300

    added a new app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,8 +3,9 @@ from relationship_app.models import *
 # query books by a given author
 books = Book.objects.filter(author=author)
 
-# list all books in library
-library = Library.objects.get(name=library_name)
+# list all books in library#
+#library = Library.objects.get(name=library_name)
+
 books = library.book.all()
 
 # books = Book.objects.filter(library=library)
