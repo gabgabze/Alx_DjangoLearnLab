ce74f910fe5b4fd59487684bbf4ad20d9d8e7123
waregagbagbo <gabelwarega@gmail.com>
2025-07-20 00:10:24 +0300

    fixing the query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,4 @@
 from relationship_app.models import *
 
-# query books by a given author
-#books = Book.objects.filter(author=author)
-
-# list all books in library#
-library = Library.objects.get(name=library_name)
-library.books.all()
-
-# books = Book.objects.filter(library=library)
-
-# librarian for library
-#librarian = Library.library
+# list all books in library
+books = Book.objects.all()
