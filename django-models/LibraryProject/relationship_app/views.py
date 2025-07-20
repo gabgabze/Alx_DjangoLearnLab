7a91b4ae0c7fb11ced38d19270766c56c2c47620
gabgabze <denzrich10@gmail.com>
2025-07-20 23:11:11 +0300

    fixing the urls and views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from relationship_app.models import *
+from .models import Book, Library
 from django.views.generic import ListView,DetailView
 # Create your views here.
 
