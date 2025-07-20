fc9703aa7de108b557cb157da4f1fbb75826f497
gabgabze <denzrich10@gmail.com>
2025-07-20 23:58:41 +0300

    fixed the urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,6 @@ from django.contrib.auth import views as auth_views
 url_patterns =[
     path('books/', views.book_list, name='home'),
     path('detail/',views.LibraryDetailView.as_view(), name='detail' ),
-
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register')
 ]
