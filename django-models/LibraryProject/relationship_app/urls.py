1ef71fbb8d83262c7509b3389d9ab8ad10ac1cde
gabgabze <denzrich10@gmail.com>
2025-07-20 23:44:07 +0300

    fixed the urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from relationship_app import views
+from .views import list_books, LibraryDetailView
 from django.contrib.auth import views as auth_views
 
 url_patterns =[
     path('books/', views.book_list, name='home'),
-    path('detail/', views.BookDetail.as_view(), name='detail' ),
+    path('detail/', views.LibraryDetailView.as_view(), name='detail' ),
+
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register'),
+    path('register/', views.register, name='register')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,12 +6,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 # Create your views here.
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request,'relationship_app/list_books.html',context)
 
-class BookDetail(DetailView):
+class ibraryDetailView(DetailView):
     model = Library
     context_object_name = 'books'
     template_name = 'relationship_app/library_detail.html'
