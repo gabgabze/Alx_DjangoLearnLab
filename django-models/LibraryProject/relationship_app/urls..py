169a3ef75d108e37acb7b8851f82f9b64c843f52
gabgabze <denzrich10@gmail.com>
2025-07-20 23:34:17 +0300

    added the template files

--- django-models/LibraryProject/relationship_app/urls..py
+++ django-models/LibraryProject/relationship_app/urls..py
@@ -1,7 +1,12 @@
 from django.urls import path
 from relationship_app import views
+from django.contrib.auth import views as auth_views
 
 url_patterns =[
     path('books/', views.book_list, name='home'),
     path('detail/', views.BookDetail.as_view(), name='detail' ),
+
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from .models import Library
+from django.shortcuts import render,redirect
+from .models import Library,Book
 from django.views.generic.detail import DetailView
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def book_list(request):
@@ -12,4 +15,17 @@ class BookDetail(DetailView):
     model = Library
     context_object_name = 'books'
     template_name = 'relationship_app/library_detail.html'
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after registration
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
 
