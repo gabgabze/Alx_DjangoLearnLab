7d3ef4987fb2c5b545a20090768520f0143673f9
gabgabze <denzrich10@gmail.com>
2025-07-20 23:17:32 +0300

    fixing the urls and views

--- django-models/LibraryProject/relationship_app/urls..py
+++ django-models/LibraryProject/relationship_app/urls..py
@@ -3,5 +3,5 @@ from relationship_app import views
 
 url_patterns =[
     path('books/', views.book_list, name='home'),
-    path('detail', views.BookDetail.as_view(), name='detail' ),
+    path('detail/', views.BookDetail.as_view(), name='detail' ),
 ]
