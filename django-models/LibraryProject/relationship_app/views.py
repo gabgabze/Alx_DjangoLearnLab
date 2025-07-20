391b013fac0ed39b49ac29058849f80b6f90110a
gabgabze <denzrich10@gmail.com>
2025-07-20 23:13:31 +0300

    fixing the urls and views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ def book_list(request):
     return render(request,'relationship_app/list_books.html',context)
 
 class BookDetail(DetailView):
-    model = Book
+    model = Library
     context_object_name = 'books'
     template_name = 'relationship_app/library_detail.html'
 
