d93935dc74e09ed7cc9cab9e12e69c1da84ede4b
gabgabze <denzrich10@gmail.com>
2025-07-22 00:16:22 +0300

    fixed the urls for the roles view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,12 +32,15 @@ def register(request):
 """ create role views"""
 #@user_passes_test(is_admin)
 def admin_view(request):
-    return render(request,'relationship_app/admin_view.html')
+    pass
+    # render(request,'relationship_app/admin_view.html')
 
 #@user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request,'relationship_app/librarian_view.html')
+    pass
+    #return render(request,'relationship_app/librarian_view.html')
 
 #@user_passes_test(is_member)
 def member_view(request):
-    return render(request,'relationship_app/member_view.html')
+    pass
+    #return render(request,'relationship_app/member_view.html')
