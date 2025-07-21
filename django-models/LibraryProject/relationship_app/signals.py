782face49bc6e25931d0736ce6cae7ce9526b7b7
gabgabze <denzrich10@gmail.com>
2025-07-21 07:23:43 +0300

    updated and fixed the signals profile file

--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -1,16 +1,14 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save  # acts when saving is on db
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import Profile
+from django.dispatch import receiver #  listen for the signal and "receive" it when it fires
+from .models import UserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-    instance.profile.save()
-    instance.save()
-    return instance
+
