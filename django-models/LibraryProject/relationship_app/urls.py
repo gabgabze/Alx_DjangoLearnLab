39aa420a7cdbd21cb31e94b8a750ca3e5c029659
gabgabze <denzrich10@gmail.com>
2025-07-21 23:35:50 +0300

    fixed the urls for the roles view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,8 @@ from .import views
 urlpatterns = [
     path('books/', views.book_list, name='home'),
     path('detail/',views.LibraryDetailView.as_view(), name='detail' ),
-    path('register/', views.register, name='register')
-    path('admin_view/', views.admin_view, name='admin_view'),
+    path('register/', views.register, name='register'),
+    path('admin_view', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian'),
     path('member/', views.member_view, name='member'),
 ]
