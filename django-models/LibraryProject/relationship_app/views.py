b8b7903721c6163084e4bcd5b7b675beb08b86af
gabgabze <denzrich10@gmail.com>
2025-07-22 11:03:43 +0300

    fixing the admin view logic

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,18 +32,18 @@ def register(request):
 """ create role views"""
 @user_passes_test(is_admin, login_url='login')
 def admin_view(request):
-    #context ={}
-    return render(request,'relationship_app/admin_view.html')
+    if request.user.is_superuser:
+        return render(request,'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian, login_url='login')
-def librarian_view(request):
-    #context = {}
-    return render(request,'relationship_app/librarian_view.html')
+def librarian_view(request, user=None):
+    if user.is_librarian:
+        return render(request,'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member, login_url='login')
-def member_view(request):
-    #context = {}
-    return render(request,'relationship_app/member_view.html')
+def member_view(request, user=None):
+    if user.is_member:
+        return render(request,'relationship_app/member_view.html')
 
 
 @permission_required(relationship_app.can_add_book,login_url='login')
