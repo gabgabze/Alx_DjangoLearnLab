5860a29645775507cb51288887561fabf70d114a
gabgabze <denzrich10@gmail.com>
2025-07-25 16:44:54 +0300

    sync changes

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,22 @@
-from django.contrib import admin
-from .models import Book
+"""from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
-
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ['email','date_of_birth','profile_photo']
+    fieldsets = [
+        ('Title', {
+            'fields': [
+                '',
+            ],
+        }),
+    ]
+# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title','author','publication_year')
     #search_fields = ('title', 'author')
     list_filter = ('publication_year',)
+"""
 
-admin.site.register(Book, BookAdmin)
