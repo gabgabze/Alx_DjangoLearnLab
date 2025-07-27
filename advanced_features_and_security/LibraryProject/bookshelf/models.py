1a55b8b563e14f180561a0fe3ea2643b03af304f
gabgabze <denzrich10@gmail.com>
2025-07-27 11:04:04 +0300

    Fixed the customUser fieldset error

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -48,3 +48,13 @@ class Editor(models.Model):
             ('can_vew','can_create'),
             ('can_edit','can_delete')
         ]
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
