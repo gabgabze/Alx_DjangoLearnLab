21913b61b41c3f8f9a1a00cfa9565147cef4732f
gabgabze <denzrich10@gmail.com>
2025-08-02 13:06:57 +0300

    added the permission classes

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -47,7 +47,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.IsAuthenticated'
     ),
+
 
 }
 
