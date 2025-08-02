f74b63893160dcb36e7dba379294225d7f162bff
gabgabze <denzrich10@gmail.com>
2025-08-02 03:48:35 +0300

    fixing the modules

--- api_project/api_project/api_project/settings.py
+++ api_project/api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nqafkl$9q6-0u*xr!-z$*nhmacfvzgg=vb2vh#%3tpzi9%vai!'
+SECRET_KEY = 'django-insecure-lf*g7)bl=zx25%#&s!@*gww(v86l#z*z)_e!bt^+nic)*oppfy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
