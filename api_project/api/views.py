1bd2e0f07d433fa76048e132c2d76ea4801271b5
gabgabze <denzrich10@gmail.com>
2025-08-02 12:06:00 +0300

    fixed the generic view

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 
 # Create your views here.
 
-class BookList(ListAPIView):
-    model = Book
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
