ce51b082e79b991b545f194667e5b93bdf699b37
gabgabze <denzrich10@gmail.com>
2025-08-10 17:31:42 +0300

    added the filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import *
 from .serializers import BookSerializer,AuthorSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
