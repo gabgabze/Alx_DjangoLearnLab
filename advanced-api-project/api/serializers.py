50408ba72cdd926f720f421c43c242ccf12314d0
gabgabze <denzrich10@gmail.com>
2025-08-10 13:11:35 +0300

    creaeted the serializer module and components

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Book, Author
+from datetime import datetime
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,6 +10,14 @@ class AuthorSerializer(serializers.ModelSerializer):
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author',)
+        fields = '__all__'
         read_only_fields = ('id',)
-        exclude = ('publication_year',)
+
+"""import datettime do the data checks against future and return data"""
+
+    def validate(self, data):
+        """get the current date"""
+        current_year = datetime.now().year
+        if data["published_year"] > current_year:
+            raise serializers.ValidationError('Publication year cannot be in future')
+        return data
