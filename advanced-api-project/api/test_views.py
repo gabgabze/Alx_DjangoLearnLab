0a1e7102ce77a66441288b8d871a478de69ad059
gabgabze <denzrich10@gmail.com>
2025-08-10 23:06:50 +0300

    created test cases for differenet views

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -3,7 +3,7 @@ from api.models import *
 
 """create a Book test case"""
 
-class BookTestCase(TestCase):
+class APITestCase(TestCase):
     def setUp(self):
         Book.objects.create("")
         Book.objects.create("")
@@ -11,6 +11,28 @@ class BookTestCase(TestCase):
         Book.objects.create("")
 
 
-class BookListTestCase(BookTestCase):
+class BookListTestCase(APITestCase):
     def setUp(self):
+        Book.objects.create("")
+
+
+class BookDetailTestCase(APITestCase):
+    def setUp(self):
+        Book.objects.create("")
+        Book.objects.create("")
+
+class APITestCase(BookTestCase):
+    def setUp(self):
+        Book.objects.create("")
+        Book.objects.create("")
+
+class BookUpdateTestCase(APITestCase):
+    def setUp(self):
+        Book.objects.create("")
+        Book.objects.create("")
+        Book.objects.create("")
+
+class BookDeleteTestCase(APITestCase):
+    def setUp(self):
+        Book.objects.create("")
         Book.objects.create("")
