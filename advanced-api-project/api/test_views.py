df47dc11543d8931d6640629b79cc0652cf34b75
gabgabze <denzrich10@gmail.com>
2025-08-10 23:03:37 +0300

    created test file

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 from api.models import *
-  
+
+"""create a Book test case"""
+
+class BookTestCase(TestCase):
+    def setUp(self):
+        Book.objects.create("")
+        Book.objects.create("")
+        Book.objects.create("")
+        Book.objects.create("")
+
+
+class BookListTestCase(BookTestCase):
+    def setUp(self):
+        Book.objects.create("")
