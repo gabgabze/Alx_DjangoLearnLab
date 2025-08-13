475b97ab6325b1ddc0b6e3cef7d23f0ea6abafe5
gabgabze <denzrich10@gmail.com>
2025-08-14 02:30:25 +0300

    fixed the import module

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from  django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.conf import settings
 #from django_blog.settings import AUTH_USER_MODEL
 
