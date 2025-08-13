943c6840adf0198a9a73e0867027a56326304ff5
gabgabze <denzrich10@gmail.com>
2025-08-14 02:17:51 +0300

    created the models, custom user and signal

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from  django.contrib.auth.models import User
+from django.conf import settings
+from django_blog.settings import AUTH_USER_MODEL
+
 
 # Create your models here.
+# create custom user
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/')
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Register the model
+AUTH_USER_MODEL = 'blog.User'
