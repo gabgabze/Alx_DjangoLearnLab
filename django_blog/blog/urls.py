d6e20da57c2478132119e300d0b845884af3cbd0
gabgabze <denzrich10@gmail.com>
2025-08-16 09:09:55 +0300

    added the comment model and related form

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,9 +17,9 @@ urlpatterns =[
 
     # crud operation  CBV for comment
     path('post/<int:pk>/comments/new', views.CommentCreateView.as_view(), name='comments'),
-    #path('post/<int:pk>/like/', views.LikePostView.as_view(), name='like'),
-    #path('post/<int:pk>/dislike/', views.DislikePostView.as_view(), name='dislike'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
-    path('comment/<int:pk>/update', views.CommentUpdateView.as_view(), name='post/<int:pk>/comments/new/'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='post/<int:pk>/comments/new/'),
+    # path('post/<int:pk>/like/', views.LikePostView.as_view(), name='like'),
+    # path('post/<int:pk>/dislike/', views.DislikePostView.as_view(), name='dislike'),
 
 ]
