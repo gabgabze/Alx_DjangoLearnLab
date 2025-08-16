7fd504fb0b5a436c761ea420478d7d856018e8aa
gabgabze <denzrich10@gmail.com>
2025-08-16 08:07:10 +0300

    updated the CRUD view ops

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .import views
+from django.views import generic
 
 urlpatterns =[
     path('login/', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('post/', views.blog, name='blog'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='blog_detail'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='blog_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='blog_delete'),
+
 ]
