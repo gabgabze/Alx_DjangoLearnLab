8f234fd638f9363731166cbff50b8413dc3807c3
gabgbaze <gabelgab99@gmail.com>
2025-08-26 01:29:00 +0300

    created the feed enpdoints

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,5 +3,5 @@ from accounts.urls import *
 from .import views
 
 url_patterns =[
-    path('/feed',views.PostUserViewSet.as_view(), name='feed'),
+    path('feed',views.PostUserViewSet.as_view(), name='feed/'),
 ]
