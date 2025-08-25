5d6de0f3642ae0412e25b458dfbe8e243b64ff13
gabgbaze <gabelgab99@gmail.com>
2025-08-26 00:33:29 +0300

    updated the m:m serializer

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -15,8 +15,13 @@ class CustomUserRegister(generics.CreateAPIView):
         serializer.save()
 
 
-class CustomFollowerViewSet(viewsets.ReadOnlyModelViewSet):
+class CustomFollowerViewSet(generics.GenericAPIView):
     serializer_class = CustomUserSerializer(many=True, read_only=True)
     queryset = CustomUser.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
+    # create a custom method overrider to handle the following loging
+    def perform_create(self, serializer):
+        follower = self.request.user.followers.all()
+        if follower:
+            serializer.save(user=self.request.user)
